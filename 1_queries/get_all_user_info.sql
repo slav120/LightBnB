889194fa27eae3b724f793706464d4ab9d24a332
Slav Lalev <53335999+slav120@users.noreply.github.com>
2019-11-13 15:25:53 -0500

    Add files via upload
    New queries to sort info out

---
+++ 1_queries/get_all_user_info.sql
@@ -0,0 +1,9 @@
+-- SELECT * 
+-- FROM users
+-- WHERE name LIKE "%Michael%"    where it has michael in name 
+-- WHERE name = "Michael" where his name is Michael 
+
+
+SELECT id, name, email, password
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
